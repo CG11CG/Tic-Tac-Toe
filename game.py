a206cf7c3480f9c065d1e8eeb2178142bd37e0f9
Cruz Gallegos <cruzmgallegos11@hotmail.com>
2024-07-26 22:46:52 -0700

    deleted files

---
+++ game.py
@@ -0,0 +1,8 @@
+def check_win(board):
+    pass
+
+def check_draw(board):
+    pass
+
+def play_turn(board, player):
+    pass
---
+++ players.py
@@ -0,0 +1,11 @@
+def validate_move(board, move):
+    pass
+
+def switch_player(current_player):
+    pass
+
+def get_move():
+    pass
+
+def main():
+    pass
